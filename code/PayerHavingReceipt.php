4ad4abe15182eb15b42ab0987f8377c8e2e8aa89
Morven Lewis-Everley <morven@i-lateral.com>
2012-10-04 13:11:18 +0100

    Add fixes to get besic SS3 support running

--- code/PayerHavingReceipt.php
+++ code/PayerHavingReceipt.php
@@ -1,18 +1,14 @@
 <?php
 
-class PayerHavingReceipt extends DataObjectDecorator{
-	function extraStatics(){
-		return array(
-			'db' => array(
-				'Street' =>		'Varchar',
-				'Suburb' =>		'Varchar',
-				'CityTown' =>	'Varchar',
-				'Country' =>	'Varchar',
-			),
-		);
-	}
+class PayerHavingReceipt extends DataExtension{
+	public static $db = array(
+		'Street' =>		'Varchar',
+		'Suburb' =>		'Varchar',
+		'CityTown' =>	'Varchar',
+		'Country' =>	'Varchar',
+	);
 	
-	function ReceiptMessage(){
+	public function ReceiptMessage(){
 		return $this->owner->renderWith('Payer_receipt');
 	}
 }
